b875af5275d2561a5428a642ca8e8914f94982a3
Mikhail Oleynik <mishutka@mito-team.com>
2022-01-16 14:37:00 +0300

    composer staff and loader

---
+++ src/MtJpGraph.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace mitoteam\jpgraph;
+
+final class MtJpGraph
+{
+  private static $_loaded_modules = array();
+
+  public static function load($modules = null)
+  {
+    // basic "module" has empty name
+
+    if(!in_array('', self::$_loaded))
+    {
+      require_once __DIR__ . '/lib/jpgraph.php';
+      self::$_loaded[] = '';
+    }
+
+    if($modules)
+    {
+      if(!is_array($modules))
+      {
+        $modules = array($modules);
+      }
+
+      foreach ($modules as $module)
+      {
+        if(in_array($module, self::$_loaded))
+        {
+          continue; // already loaded
+        }
+
+        $filename = __DIR__ . '/lib/jpgraph_' . $module . '.php';
+
+        if(!file_exists($filename))
+        {
+          throw new \Exception("Module '$filename' not found");
+        }
+
+        require_once $filename;
+        self::$_loaded[] = $module;
+      }
+    }
+  }
+}
